b7c5c20cf898b6bae9bc3e5aaebefd5a11a6f547
Trible69 <edwinamunoz357@gmail.com>
2024-07-11 13:17:05 -0500

    Rayan Omar Image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,7 +9,8 @@ export default function HomeScreen() {
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
-        <Image  
+        <Image  source={require('@/assets/images/RayanOmar.jpg')}
+        style={styles.rayan_omar}
         />
       }>
       <ThemedView style={styles.titleContainer}>
@@ -20,7 +21,8 @@ export default function HomeScreen() {
         <ThemedText>
           "But I...Wanna be a Hero Too! Like Rayan Omar!" - Dylan Hoang 2023{"\n\n"}
           "I will kill Rayan Omar!" - Kenneth Khoi Nguyen-Tran 1986{"\n\n"}
-          "He is My Hero Academia!" - Carson Garner 2024
+          "He is My Hero Academia!" - Carson Garner 2024{"\n\n"}
+          "he's gang even tho he hates me :fireemoji:" - Gabriella Jane Gibler 2025
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
@@ -43,5 +45,9 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: 'absolute',
+  },
+  rayan_omar: {
+    width: 400,
+    height: 500,
   },
 });
