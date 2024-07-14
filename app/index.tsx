c433d5efcc99d5a2c35565fe5ff4afaa6e998c1c
Trible69 <edwinamunoz357@gmail.com>
2024-07-14 10:08:53 -0500

    hngbdfvdcvfbdgn

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,21 @@
 import { Text, View } from "react-native";
+import express from 'express';
+import sequelize from '../routes/routes.js';
+import router from 'utils/database'
+
+const app=express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use((_, res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
+
+app.use(router);
+sequelize.sync();
+app.listen(5000);
 
 export default function Index() {
   return (
